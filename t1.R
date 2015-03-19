51de9af21035a4846c4d36e39061c066e8466c8a
slakov <slakov@gmail.com>
2015-03-19 16:36:03 +0100

    task 1 complete

--- t1.R
+++ t1.R
@@ -2,12 +2,12 @@
 
 fileName <- "test_file"
 
-
 #Create file connection
 con <- file(fileName, "r")
-#Read file line by line
-#d <- readLines(con)
+
+#Read all words
 d <- scan(con, character(0))
+
 # Close connection
 close(con)
 
@@ -15,11 +15,44 @@ close(con)
 con <- file("bad_words", "r")
 bad <- scan(con, character(0))
 close(con)
+##
 
+d <- lapply(d, toupper)
+bad <- lapply(bad, toupper)
 
-l <- lapply(d, toupper)
+# create out vector of words
+words <- c()
+numbers <- c()
+puncts <- c()
 
-#Split by space
-#wordsSplit <- strsplit(d, split=" ")
+for (i in (1:100)){
+  
+  element <- d[[i]]
+  # exstract word 
+  word <- gsub("[^A-Z]+", "", element)
+  
+  # filter out bad words
+  good <-1
+  if (word %in% bad) {good <-0}
+  if ((word > 0)&good){words <- c(words, word)}
+  
+  # extract numbers
+  number <- gsub("[^0-9]+", "", element)
+  if (number > 0){numbers <- c(numbers, number)}
+  
+  # extract punctuation
+  pun <- gsub("[^[:punct:]]", "", element)
+  print(element)
+  if (nchar(pun) > 0){puncts <- c(puncts, pun)}
+  
+  
+  
+}
 
-#words <- grep("[a-z]", wordsSplit, value=T)
+# exploration of frequencies
+#tab <- table(words[which(nchar(words)>10)])
+#df <- as.data.frame(tab)
+#names(df) <- c("word","times")
+#df$rank <- rank(-df$times, ties.method="min")
+#df <- df[order(df$rank,decreasing = F),]
+#print(df[(1:30),])
